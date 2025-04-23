9342dfc83d99969e8a487a0fe87dc69e14fe990e
NikDm <respectedoff@yandex.ru>
2025-04-23 18:00:35 +0300

    hwork

---
+++ script.js
@@ -0,0 +1,7 @@
+const city = "Yalta"; /* город */
+const temperature = "+22"; /* температура */
+const mathExp = (2 + 2) * 2; /* мат выражение */
+
+console.log(city);
+console.log(temperature);
+console.log(mathExp);
