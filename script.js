a8066c5bd144bbd43e11f12f88051514990986f4
NikDm <respectedoff@yandex.ru>
2025-04-23 18:09:06 +0300

    less3

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-const city = "Yalta"; /* город */
-const temperature = "+22"; /* температура */
-const mathExp = (2 + 2) * 2; /* мат выражение */
+let price = 100;
+price = 150;
 
-console.log(city);
-console.log(temperature);
-console.log(mathExp);
+console.log(price);
