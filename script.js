c004e9e60f1f55c27b5c5ab4f43372dd142991c5
NikDm <respectedoff@yandex.ru>
2025-04-23 18:31:41 +0300

    less4

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-const city = "Yalta"; /* город */
-const temperature = "+22"; /* температура */
-const mathExp = (2 + 2) * 2; /* мат выражение */
-
-console.log(city);
-console.log(temperature);
-console.log(mathExp);
+const str = "hello";
+const num = 15;
+const bool = true;
+const empty = null;
+const undefinedValue = undefined;
+const obj = {
+  name: "Nik",
+  age: 10,
+};
+const arr = [1, 2, "a", true, null];
+const func = () => {
+  console.log("hello");
+};
+console.log(typeof str);
+console.log(typeof num);
+console.log(typeof bool);
+console.log(typeof null);
+console.log(typeof undefined);
+console.log(typeof obj);
+console.log(typeof arr);
+console.log(typeof func);
