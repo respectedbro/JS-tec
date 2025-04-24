ab1c7583f2a85677661391ab85e3a65ef7ec8917
NikDm <respectedoff@yandex.ru>
2025-04-24 14:52:14 +0300

    less5

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
-const str = "Hello";
-const num = 12;
-const bool = false;
-const empty = null;
-const und = undefined;
-const arr = [1, 2, "q", null];
-const obj = {
-  a: 11,
-  b: "foo",
-};
-const sym = Symbol();
+// 1)
+const dig = 2 * 2 + 2;
 
-console.log(typeof str);
-console.log(typeof num);
-console.log(typeof bool);
-console.log(typeof empty);
-console.log(typeof und);
-console.log(typeof arr);
-console.log(typeof obj);
-console.log(typeof sym);
+const rad54 = (54 * Math.PI) / 180;
+const rad16 = (16 * Math.PI) / 180;
+const result = Math.pow(Math.sin(rad54) * Math.cos(rad16), 2);
+
+const exp =
+  (16 * Math.sqrt(13.2 * 71.9)) / 2.4 / 7 ** 4 + 3 ** Math.sqrt(49) * 2 ** 7;
+
+console.log(dig);
+console.log(result);
+console.log(exp);
+
+// 2
+const num1 = 10;
+const num2 = 15;
+const num3 = -4;
+
+if (num1 % 2 === 0) {
+  console.log("чётное");
+} else {
+  console.log("нечётное");
+}
+
+console.log(num2 % 2 === 0 ? "чётное" : "нечётное");
+
+console.log(num3 % 2 === 0 ? "чётное" : "нечётное");
+
+// 3)
+
+const name = "";
+
+console.log(name === "" ? "Hello, Guest!" : `Hello, ${name}!`);
