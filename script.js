814356f01e7e283e61eeaec6ca7171c4ad3934d4
resAsus <respectedbro@yandex.ru>
2025-04-25 17:16:31 +0300

    less9

--- script.js
+++ script.js
@@ -1,24 +1,51 @@
 // 1
-const str = 'JavaScript is fun!';
-console.log(str.includes('fun'));
+const arr = [1, 2, 3, 4, 5];
 
-// 2
-let age;
+const arrDouble = arr.map((item) => {
+    return item * 2;
+})
 
-if (age === null || age === undefined) {
-    console.log(':(');
+console.log(arrDouble);
+
+//2
+const arr1 = [1, 2, 2, 3, 4, 5, 5, 5, 6];
+const arrFilter = arr1.filter((item, index) => arr1.indexOf(item) === index);
+console.log(arrFilter);
+
+//3
+const arr3 = [1, 2, 3];
+
+const arrReduce = arr3.reduce((total, sum) => {
+    return total + sum;
+}, 0)
+
+console.log(arrReduce)
+
+//4
+const arr4 = [1, 2, 3, 4, 5, 6];
+const reverse = [];
+
+for (let i = arr.length - 1; i >= 0; i--) {
+    reverse.push(arr[i]);
 }
-// 3
-const firstName = 'John';
-const lastName  = 'Doe';
-const occupation = 'software developer'
 
-console.log(`Hello, my name is ${firstName} ${lastName}. I am a ${occupation}.`)
+console.log(reverse);
 
-// 4
-console.log(null === undefined) // здесь сравнивается значение и тип null - object, undefined - undefined)
-console.log(null == undefined) // без строгого сравнения null и undefined равны только друг другу
+//5
 
-// 5
+// если массив или обект лежит в перемненной объявленной через const его нельзя переопределить, но содержимое можно изменять, так как значение не меняется
 
-console.log(1 + '1') // если хотя бы один аргумент строка при сложении js неявно приводит число аргумент к строке
+const str1 = 'sdsfds';
+let str2 = 'asdffg';
+
+str1 ='aaas'; // значение нельзя переопределить
+str2 = 'sdsdsd'; // можно
+
+const array1 = [1,2,3,4,5];
+let array2 = [1,2,3,4,5];
+
+array1.push(10); // можно изменить
+array2.push(20); // можно изменить
+
+array1 = [1,2,5,6,9,43]; // нельзя переопределить значение
+array2 = [1,2,34,45,66]; // можно
