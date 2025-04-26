013e8d34c0a4b5ac998106b79a7a1220a7edda5a
resAsus <respectedbro@yandex.ru>
2025-04-26 16:11:11 +0300

    less10

--- script.js
+++ script.js
@@ -1,51 +1,61 @@
 // 1
-const arr = [1, 2, 3, 4, 5];
-
-const arrDouble = arr.map((item) => {
-    return item * 2;
-})
-
-console.log(arrDouble);
-
-//2
-const arr1 = [1, 2, 2, 3, 4, 5, 5, 5, 6];
-const arrFilter = arr1.filter((item, index) => arr1.indexOf(item) === index);
-console.log(arrFilter);
-
-//3
-const arr3 = [1, 2, 3];
-
-const arrReduce = arr3.reduce((total, sum) => {
-    return total + sum;
-}, 0)
-
-console.log(arrReduce)
-
-//4
-const arr4 = [1, 2, 3, 4, 5, 6];
-const reverse = [];
-
-for (let i = arr.length - 1; i >= 0; i--) {
-    reverse.push(arr[i]);
+const averageValue = (...nums) => {
+    const sum = nums.reduce((acc, num) => acc + num, 0);
+    return sum / nums.length
 }
 
-console.log(reverse);
+console.log(averageValue(1, 2,3,4))
+
+// 2
+const user = {
+    name: 'Ad',
+    age: 33,
+    country: 'Rus'
+};
+
+const userInfo = ({name, age, country}) => {
+    console.log(name, age, country)
+}
+
+userInfo(user)
+
+// 3
+const obj = {
+    name: 'Ab',
+    arr: [1, 2, 3, 4],
+    user: {
+        age: 20,
+        position: 'developer'
+    }
+};
+
+const {name, arr: [first, ...rest], user: { age, position }} = obj
+console.log(name)
+console.log(first)
+console.log(rest)
+console.log(age)
+console.log(position)
+
+// 4
+const array = [1, 2, 3, 4, 5]
+
+const newArray = [...array]
+
+newArray.unshift(0)
+newArray.push('item')
+
+console.log(array)
+console.log(newArray)
 
 //5
+const object = {
+    id: 2,
+    name: 'foo',
+    position: 'dev'
+}
 
-// если массив или обект лежит в перемненной объявленной через const его нельзя переопределить, но содержимое можно изменять, так как значение не меняется
+const paramsFunc = ({id, country, ...rest}) => {
+    return rest
+}
 
-const str1 = 'sdsfds';
-let str2 = 'asdffg';
-
-str1 ='aaas'; // значение нельзя переопределить
-str2 = 'sdsdsd'; // можно
-
-const array1 = [1,2,3,4,5];
-let array2 = [1,2,3,4,5];
-
-array1.push(10); // можно изменить
-array2.push(20); // можно изменить
-
-array1 = [1,2,5,6,9,43]; // нельзя переопределить значение
-array2 = [1,2,34,45,66]; // можно
+console.log(paramsFunc(object))
