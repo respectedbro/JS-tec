1973a138cd29c903b4e03293b3da884a142db635
NikDm <respectedoff@yandex.ru>
2025-04-29 21:46:58 +0300

    less11

--- script.js
+++ script.js
@@ -1,32 +1,61 @@
-// 1
-// let num = 1;
-//
-// for (let i = num; i <= 10; i++) {
-//   console.log(i);
-// }
+const isPalindrome = (str) => {
+  return str === str.split("").reverse().join("");
+};
 
-// 2
-// let sum = 0;
-// let i = 1;
-//
-// while (i <= 100) {
-//   sum += i;
-//   i++;
-// }
-// console.log(sum);
+console.log(isPalindrome("топот"));
 
-// 3
-for (let num = 2; num <= 100; num++) {
-  let isPrime = true;
+const shortestWordInSentence = (sentence) => {
+  const str = sentence.split(" ");
+  let shortestWord = str[0];
 
-  for (let i = 2; i <= Math.sqrt(num); i++) {
-    if (num % i === 0) {
-      isPrime = false;
-      break;
+  str.forEach((word) => {
+    if (word.length < shortestWord.length) {
+      shortestWord = word;
+    }
+  });
+  return shortestWord;
+};
+
+console.log(shortestWordInSentence("тоже хороший вариант"));
+
+function formatPhNumber(num) {
+  let strNum = String(num);
+  let cleanNum = "";
+
+  for (let num of strNum) {
+    if ("0123456789".includes(num)) {
+      cleanNum += num;
     }
   }
 
-  if (isPrime) {
-    console.log(num);
-  }
+  const code = cleanNum.slice(1, 4);
+  const fst = cleanNum.slice(4, 7);
+  const sec = cleanNum.slice(7, 10);
+
+  return `8 (${code}) ${fst}-${sec}`;
 }
+
+console.log(formatPhNumber(121212121212));
+
+const minMaxValue = (arr) => {
+  return {
+    min: Math.min(...arr),
+    max: Math.max(...arr),
+  };
+};
+
+console.log(minMaxValue([5, 2, 8, 1, 4]));
+
+function sortArray(arr) {
+  const sortedArr = [...arr];
+  for (let i = 0; i < sortedArr.length; i++) {
+    for (let j = 0; j < sortedArr.length - 1 - i; j++) {
+      if (sortedArr[j] > sortedArr[j + 1]) {
+        [sortedArr[j], sortedArr[j + 1]] = [sortedArr[j + 1], sortedArr[j]];
+      }
+    }
+  }
+  return sortedArr;
+}
+console.log(sortArray([5, 3, 8, 1]));
+console.log(sortArray([-10, 0, 4, -2]));
