b0edbe72a03b64ed28c17da66bc173fc6322a7b8
resAsus <respectedbro@yandex.ru>
2025-04-30 11:42:23 +0300

    lesss12

--- code-style.js
+++ code-style.js
@@ -1,6 +1,13 @@
 // 1 задание
 // Возьмите следующий код и приведите его в соответствие с общепринятым стандартом форматирования,
 // соблюдая отступы, выравнивание и правила расстановки пробелов:
+// function multiply(a,b){
+//     return a*b;
+// }
+// const person ={name:'Alice',age:30};
+// if(person.age>18){console.log('Adult');}
+// else{console.log('Minor');}
+
 function multiply(a, b) {
     return a * b;
 }
@@ -15,6 +22,12 @@ if (person.age > 18) {
 // 2 задание
 // Представьте, что вы работаете в команде, и вам нужно сделать код понятным для всех участников.
 // Перепишите следующий код, используя понятные и логичные имена переменных и функций:
+// function x(a, b) {
+//     let c = a * b;
+//     return c;
+// }
+// let d = x(5, 10);
+
 function sumOfItems(a, b) {
     let sum = a * b;
     return sum;
@@ -25,6 +38,13 @@ let itemValue = sumOfItems(5, 10);
 // 3 задание
 // Убедитесь, что в коде используется единый стиль оформления. В следующем коде присутствуют смешанные стили кавычек,
 // разное использование var, let, и const, а также различное форматирование объектов и массивов. Исправьте код:
+// let items = ["apple", 'banana', "orange"];
+// let price = {apple: 1, banana: 2, orange:3 };
+// const total = price['apple'] + price["banana"] + price.orange;
+//
+// function calculateTotal(items) {
+//     return items.reduce(function(total, item) {return total + item.price; }, 0); }
+
 const items = [
     'apple',
     'banana',
