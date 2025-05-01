63c4dcde9699acb843fe9a8459712ad3699f15c1
NikDm <respectedoff@yandex.ru>
2025-05-01 11:51:46 +0300

    less14

--- script.js
+++ script.js
@@ -1,61 +1,21 @@
-const isPalindrome = (str) => {
-  return str === str.split("").reverse().join("");
+//1
+const person = {
+  name: "Nik",
+  age: 35,
+  occupation: "developer",
+  hobbies: ["sport", "gaming", "walk"],
 };
 
-console.log(isPalindrome("топот"));
-
-const shortestWordInSentence = (sentence) => {
-  const str = sentence.split(" ");
-  let shortestWord = str[0];
-
-  str.forEach((word) => {
-    if (word.length < shortestWord.length) {
-      shortestWord = word;
-    }
-  });
-  return shortestWord;
-};
-
-console.log(shortestWordInSentence("тоже хороший вариант"));
-
-function formatPhNumber(num) {
-  let strNum = String(num);
-  let cleanNum = "";
-
-  for (let num of strNum) {
-    if ("0123456789".includes(num)) {
-      cleanNum += num;
-    }
-  }
-
-  const code = cleanNum.slice(1, 4);
-  const fst = cleanNum.slice(4, 7);
-  const sec = cleanNum.slice(7, 10);
-
-  return `8 (${code}) ${fst}-${sec}`;
+for (let key in person) {
+  console.log(key, person[key]);
 }
 
-console.log(formatPhNumber(121212121212));
+console.log(Object.keys(person));
+console.log(Object.values(person));
+console.log(Object.entries(person));
 
-const minMaxValue = (arr) => {
-  return {
-    min: Math.min(...arr),
-    max: Math.max(...arr),
-  };
-};
-
-console.log(minMaxValue([5, 2, 8, 1, 4]));
-
-function sortArray(arr) {
-  const sortedArr = [...arr];
-  for (let i = 0; i < sortedArr.length; i++) {
-    for (let j = 0; j < sortedArr.length - 1 - i; j++) {
-      if (sortedArr[j] > sortedArr[j + 1]) {
-        [sortedArr[j], sortedArr[j + 1]] = [sortedArr[j + 1], sortedArr[j]];
-      }
-    }
-  }
-  return sortedArr;
-}
-console.log(sortArray([5, 3, 8, 1]));
-console.log(sortArray([-10, 0, 4, -2]));
+//2
+person.weight = 75;
+person.hobbies[0] = "relax";
+delete person.age;
+console.log(person);
