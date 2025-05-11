0ad872c1e0392220769bc5ea0387449d0dbed766
resAsus <respectedbro@yandex.ru>
2025-05-11 16:48:31 +0300

    less21

--- script.js
+++ script.js
@@ -1,27 +1,64 @@
 //1
-const calcNumbers = numbers => numbers.map(num => num * 2);
-
-const arr = [1, 2, 3, 4, 5];
-const res = calcNumbers(arr);
-console.log(res);
-
-//2
-const user = {
+const user1 = {
     name: 'userName',
-    userSay:  () => {
-        setTimeout(() => {
-            console.log('Hello');
-        }, 1000);
+    age: 22,
+    sayHello() {
+        console.log(`${this.name} say hello `);
     }
-
 };
 
-user.userSay();
+const userSay = user1.sayHello;
+userSay();
+//this теряет связь с объектом user(привязывается к глобальному window) и становится undefined
+
+//2
+const student = {
+    name: 'Alice',
+    greet: function () {
+        console.log(`Hello, ${this.name}!`);
+    },
+    delayedGreet: function () {
+        //this внутри greet теряет связь с student
+        // setTimeout( this.greet, 1000);
+        // console.log(this)
+        setTimeout(() => this.greet(), 1000);
+    }
+};
+
+student.greet(); // Hello, Alice
+student.delayedGreet(); // Hello, undefined
 
 //3
+const person = {
+    name: 'Nik'
+};
+const logPerson = function (city, age) {
+    console.log(`меня зовут ${this.name}, я из города ${city}, мой возраст ${age}`);
+};
 
-const transformArray = (transformFunc, arr) =>  transformFunc(arr);
 
-console.log(transformArray(calcNumbers, arr));
+logPerson.call(person, 'Ялта', 33);
+logPerson.apply(person, ['Москва', 22]);
+const bindLogPerson = logPerson.bind(person);
+bindLogPerson('St.Petersburg', 29);
 
+//4 Что будет в консоли в результате выполнения функций sayHelloToAdmin() и sayHelloToUser()? Объясни, почему так произошло. Как это можно изменить?
+//     2 раза выведится bob так как bind привязывается жестко и контекст фиксируется навсегда при первом вызове.
+function sayHello() {
+    console.log('Hello, ' + this.name);
+}
+
+const admin = {
+    name: 'Bob'
+};
+
+const user = {
+    name: 'John'
+};
+
+const sayHelloToAdmin = sayHello.bind(admin);
+sayHelloToAdmin();
+
+const sayHelloToUser = sayHelloToAdmin.bind(user);
+sayHelloToUser();
 
