af91914510df12bc53ab6a4f309c6117fd062dcc
resAsus <respectedbro@yandex.ru>
2025-05-15 17:05:32 +0300

    less25

--- script.js
+++ script.js
@@ -1,45 +1,43 @@
 //1
-const calcArrElems = (arr, idx = 0) => {
-    if (idx >= arr.length) {
-        return 0;
+const safeDivide = (num1, num2) => {
+    try {
+        if (num2 === 0) {
+            throw new Error('Деление на ноль невозможно');
+        }
+        return num1 / num2;
+    } catch (error) {
+        console.log(error);
+        return null;
     }
-    return arr[idx] + calcArrElems(arr, idx + 1);
 };
-
-console.log(calcArrElems([1, 2, 3, 4, 5]));
+console.log(safeDivide(12, 0));
 
 //2
 
-const findMaxValue = (arr, idx = 0, max = 0) => {
-    if (idx >= arr.length) {
-        return max;
+const user = '{name: "Nk"}';
+const transfromJSON = (jsonStr) => {
+    try {
+        return JSON.parse(jsonStr);
+    } catch (error) {
+        console.log(error.message);
+        return null;
     }
-
-    const currMax = arr[idx] > max ? arr[idx] : max;
-
-    return findMaxValue(arr, idx + 1, currMax);
 };
 
-console.log(findMaxValue([1, 2, 3, 4, 5]));
+console.log(transfromJSON(user));
 
 //3
 
-function fibonacci() {
-    const cache = {};
-
-    const fib = (n) => {
-        if (n in cache) {
-            return cache[n];
+const checkAccess = (age) => {
+    try {
+        if (age >= 18) {
+            return 'залетай';
         }
+        throw new Error('Доступ запрещен');
+    } catch (error) {
+       console.log(error);
+        return false;
+    }
+};
 
-        if (n <= 1) {
-            return n;
-        }
-        cache[n] = fib(n - 1) + fib(n - 2);
-        return cache[n];
-    };
-    return fib;
-}
-
-const getFib = fibonacci();
-console.log(getFib(10));
+console.log(checkAccess(17));
