9e4a538c5dacb8a0f6c7494922c295136383b4bc
resAsus <respectedbro@yandex.ru>
2025-05-15 14:41:11 +0300

    cache+

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //1
 const calcArrElems = (arr, idx = 0) => {
-  if (idx >= arr.length) {
-    return 0;
-  }
-  return arr[idx] + calcArrElems(arr, idx + 1);
+    if (idx >= arr.length) {
+        return 0;
+    }
+    return arr[idx] + calcArrElems(arr, idx + 1);
 };
 
 console.log(calcArrElems([1, 2, 3, 4, 5]));
@@ -11,13 +11,13 @@ console.log(calcArrElems([1, 2, 3, 4, 5]));
 //2
 
 const findMaxValue = (arr, idx = 0, max = 0) => {
-  if (idx >= arr.length) {
-    return max;
-  }
+    if (idx >= arr.length) {
+        return max;
+    }
 
-  const currMax = arr[idx] > max ? arr[idx] : max;
+    const currMax = arr[idx] > max ? arr[idx] : max;
 
-  return findMaxValue(arr, idx + 1, currMax);
+    return findMaxValue(arr, idx + 1, currMax);
 };
 
 console.log(findMaxValue([1, 2, 3, 4, 5]));
@@ -25,11 +25,20 @@ console.log(findMaxValue([1, 2, 3, 4, 5]));
 //3
 
 function fibonacci(n) {
-  if (n <= 1) {
-    return n;
-  } else {
-    return fibonacci(n - 1) + fibonacci(n - 2);
-  }
+    const cache = {};
+
+    const fib = (n) => {
+        if (n in cache) {
+            return cache[n];
+        }
+
+        if (n <= 1) {
+            return n;
+        }
+        cache[n] = fib(n) + fib(n - 2);
+        return cache[n];
+    };
+    return fib();
 }
 
 console.log(fibonacci(10));
