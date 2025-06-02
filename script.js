94df251ff9df1d086962ea95a08f2b2ff0a64752
NikDm <respectedoff@yandex.ru>
2025-06-02 13:31:15 +0300

    less30

--- script.js
+++ script.js
@@ -1,45 +1,35 @@
-//1
-const calcArrElems = (arr, idx = 0) => {
-    if (idx >= arr.length) {
-        return 0;
-    }
-    return arr[idx] + calcArrElems(arr, idx + 1);
-};
+//1//2
+const elem1 = document.querySelector(".div1");
+const elem2 = document.querySelector(".div2");
+const elem3 = document.querySelector(".div3");
 
-console.log(calcArrElems([1, 2, 3, 4, 5]));
-
-//2
-
-const findMaxValue = (arr, idx = 0, max = 0) => {
-    if (idx >= arr.length) {
-        return max;
-    }
-
-    const currMax = arr[idx] > max ? arr[idx] : max;
-
-    return findMaxValue(arr, idx + 1, currMax);
-};
-
-console.log(findMaxValue([1, 2, 3, 4, 5]));
+elem1.addEventListener("click", () => {
+  console.log("parent");
+});
+elem2.addEventListener("click", (e) => {
+  console.log("child-1");
+  e.stopPropagation();
+});
+elem3.addEventListener("click", () => {
+  console.log("child-2");
+});
 
 //3
+const form = document.getElementById("myForm");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
 
-function fibonacci() {
-    const cache = {};
+form.addEventListener("input", (e) => {
+  if (e.target.tagName === "INPUT") {
+    const currentError = document.getElementById(`${e.target.id}Error`);
 
-    const fib = (n) => {
-        if (n in cache) {
-            return cache[n];
-        }
-
-        if (n <= 1) {
-            return n;
-        }
-        cache[n] = fib(n - 1) + fib(n - 2);
-        return cache[n];
-    };
-    return fib;
-}
-
-const getFib = fibonacci();
-console.log(getFib(10));
+    if (e.target.value.length > 20) {
+      currentError.style.display = "block";
+      e.target.style.borderColor = "red";
+    } else {
+      currentError.style.display = "none";
+      e.target.style.borderColor = "";
+    }
+  }
+});
