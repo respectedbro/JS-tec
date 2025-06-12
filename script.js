3429ba7ecd9fb678be44fa59034bf49340abc90c
resAsus <respectedbro@yandex.ru>
2025-06-12 19:29:21 +0300

    less34

--- script.js
+++ script.js
@@ -1,44 +1,81 @@
-"use strict";
 //1
-// const func = (a, b) => {
-//   "use strict";
-//   c = a + b;
-//   console.log(c);
-// };
+class Book {
+    constructor(title, author, pages) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+    }
 
-const func = (a, b) => {
-  const c = a + b;
-  console.log(c);
-};
+    bookInfo() {
+        console.log(`Название книги: ${this.title}`);
+        console.log(`Автор: ${this.author}`);
+        console.log(`Кол-во страниц: ${this.pages}`);
+    }
+}
 
-func(1, 2);
+const book = new Book('LearnJS', 'Author', 300);
+
+book.bookInfo();
 
 //2
-// const func2 = (b, b) => {
-//   "use strict";
-//   return b + b;
-// };
-//
-// console.log(func2(1, 2));
+class User {
+    constructor(name, email) {
+        this.name = name;
+        this.email = email;
+    }
 
-const func2 = (b, a) => {
-  return b + a;
-};
+    displayInfo() {
+        console.log(`Имя: ${this.name}, email: ${this.email}`);
+    }
+}
 
-console.log(func2(1, 2));
+const user1 = new User('Nik', 'example@gmail.com');
+const user2 = new User('Alex', 'example@outlook.com');
+user1.displayInfo();
+user2.displayInfo();
 
 //3
+class Rectangle {
+    constructor(width, height) {
+        this._width = width;
+        this.height = height;
+    }
 
-// function func3() {
-//   console.log(this);
-// };
+    get area() {
+        return this.width * this.height;
+    }
 
-function func3() {
-  console.log(this);
+    set width(value) {
+        if (value <= 0) {
+            console.error('Ширина должна быть положительным числом.');
+        } else {
+            this._width = value;
+        }
+    }
+
+    get width() {
+        return this._width;
+    }
+
+    get perimeter() {
+        return this.width + this.height * 2;
+    }
+
+    get height() {
+        return this._height;
+    }
+
+    set height(val) {
+        val <= 0 ? console.log('Высота должна быть положительным числом') : this._height = val;
+    }
 }
-func3();
 
-//4
-const obj = {};
-delete obj.toString;
-delete Object.prototype.toString();
+const myRect = new Rectangle(5, 10);
+console.log(myRect.area); // 50
+myRect.width = -3; // Ширина должна быть положительным числом.
+console.log(myRect.width); // 5
+
+myRect.height = -5;
+console.log(myRect.perimeter);
+console.log(myRect.height);
+
